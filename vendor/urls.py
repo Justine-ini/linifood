fc6a0bb0e219ca2f9c5696b899276d60b94fbd52
Justine-ini <inijustine4040@gmail.com>
2024-05-25 01:09:54 +0100

    menu builder, category crud

--- vendor/urls.py
+++ vendor/urls.py
@@ -5,4 +5,11 @@ from accounts import views as Accountviews
 urlpatterns = [
   path("", Accountviews.vendorDashboard, name="vendor"),
   path("profile/", views.vprofile, name="vprofile"),
+  path("menu_builder/", views.menu_builder, name="menu_builder"),
+  path("menu_builder/category/<int:pk>/", views.fooditems_by_category, name="fooditems_by_category"),
+
+  # Category CRUD
+  path("menu_builder/category/add/", views.add_category, name="add_category"),
+  path("menu_builder/category/edit/<int:pk>/", views.edit_category, name="edit_category"),
+  path("menu_builder/category/delete/<int:pk>/", views.delete_category, name="delete_category"),
 ]
